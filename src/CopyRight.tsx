18f681f925d73957d17b2d086231a818fdd6ed02
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-05-27 16:18:55 +0500

    update copyroight

--- src/CopyRight.tsx
+++ src/CopyRight.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 const CopyRight = () => {
   return (
     <div className="dark:text-gray-300 text-gray-600 text-xs text-center ">
       ChatGPT can make mistakes. Consider checking important information. Read
-      our <span className="hover:cursor-pointer underline">Terms</span> and{" "}
+      our <span className="hover:cursor-pointer underline">Terms</span> and{' '}
       <span className="hover:cursor-pointer underline">Privacy Policy</span>.
     </div>
   );
